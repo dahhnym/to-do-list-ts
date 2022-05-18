4c2e9473fbaec838aff73186fd90f0a192273613
Danim Kim <kdn725@gmail.com>
2022-05-19 00:38:32 +0900

    ✨ to-do list component

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useRecoilValue, useRecoilState } from 'recoil';
+import { categoryState, toDoSelector } from '../atoms';
+import CreateToDo from './CreateToDo';
+import ToDo from './ToDo';
+
+const ToDoList = () => {
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
+      <CreateToDo />
+      {toDos.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+    </div>
+  );
+};
+
+export default ToDoList;
